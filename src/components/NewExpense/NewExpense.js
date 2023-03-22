5d881b17f363cae1bcb5fbe2c725fb4c9bc17489
mstfay <aymustafa.32@gmail.com>
2023-03-23 00:29:20 +0300

    added new coniditions for showing new expenses in form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+    const [addNewExpense, setNewExpense] = useState(false);
+
+    
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setNewExpense(false);
     }
+
+    const showNewExpenseForm = () => {
+        setNewExpense(true);
+    }
+
+    const closeNewExpenseForm = () => {
+        setNewExpense(false);
+    }
+
+    let expenseBar = addNewExpense === true ? 
+    <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={closeNewExpenseForm} /> : <button onClick={showNewExpenseForm}>Add New Expense</button>;
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {expenseBar}
         </div>
     )
 };
